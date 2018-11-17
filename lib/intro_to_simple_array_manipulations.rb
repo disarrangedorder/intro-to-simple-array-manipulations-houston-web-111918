fef530ad65a8982c9bddd82395569511742a84e8
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 06:38:52 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,3 +14,13 @@ def pop_with_args (array, i=2)
   last_two = array.pop(i)
   return last_two
 end
+
+def using_shft (array)
+  array.shift
+end
+
+def shift_with_args (array, i=last_two)
+  array.shift(i)
+end
+  
+  
